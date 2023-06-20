4995bee14bc337d6f66ef75dce614c054bddb29a
Ayan De <deayan252@gmail.com>
2023-06-21 00:22:17 +0530

    "mediaplayer on completion functionality added"

--- app/src/main/java/com/example/firstapp/MainActivity1.java
+++ app/src/main/java/com/example/firstapp/MainActivity1.java
@@ -22,18 +22,18 @@ public class MainActivity1 extends AppCompatActivity {
 
         final ArrayList<TeamIndia> teamIndia = new ArrayList<TeamIndia>();
 
-        teamIndia.add(new TeamIndia("Virat Kohli","Batsman",R.drawable.number_one,R.raw.number_one));
-        teamIndia.add(new TeamIndia("Shewag","Batsman",R.drawable.number_two,R.raw.number_two));
-        teamIndia.add(new TeamIndia("Sachin","Batsman",R.drawable.number_three,R.raw.number_three));
-        teamIndia.add(new TeamIndia("Jadeja","All ROunder",R.drawable.number_four,R.raw.number_four));
-        teamIndia.add(new TeamIndia("Zaheer","Steamer",R.drawable.number_five,R.raw.number_five));
-        teamIndia.add(new TeamIndia("Praveen","Steamer",R.drawable.number_six,R.raw.number_six));
-        teamIndia.add(new TeamIndia("Dhoni","WK",R.drawable.number_seven,R.raw.number_seven));
-        teamIndia.add(new TeamIndia("Bhuvi","Steamer",R.drawable.number_eight,R.raw.number_eight));
-        teamIndia.add(new TeamIndia("Rishabh Pant","Batsman",R.drawable.number_nine,R.raw.number_nine));
-        teamIndia.add(new TeamIndia("Ganguly","Captain",R.drawable.number_ten,R.raw.number_ten));
+        teamIndia.add(new TeamIndia("Virat Kohli", "Batsman", R.drawable.number_one, R.raw.number_one));
+        teamIndia.add(new TeamIndia("Shewag", "Batsman", R.drawable.number_two, R.raw.number_two));
+        teamIndia.add(new TeamIndia("Sachin", "Batsman", R.drawable.number_three, R.raw.number_three));
+        teamIndia.add(new TeamIndia("Jadeja", "All ROunder", R.drawable.number_four, R.raw.number_four));
+        teamIndia.add(new TeamIndia("Zaheer", "Steamer", R.drawable.number_five, R.raw.number_five));
+        teamIndia.add(new TeamIndia("Praveen", "Steamer", R.drawable.number_six, R.raw.number_six));
+        teamIndia.add(new TeamIndia("Dhoni", "WK", R.drawable.number_seven, R.raw.number_seven));
+        teamIndia.add(new TeamIndia("Bhuvi", "Steamer", R.drawable.number_eight, R.raw.number_eight));
+        teamIndia.add(new TeamIndia("Rishabh Pant", "Batsman", R.drawable.number_nine, R.raw.number_nine));
+        teamIndia.add(new TeamIndia("Ganguly", "Captain", R.drawable.number_ten, R.raw.number_ten));
 
-        TeamAdapter Adapter = new TeamAdapter(this,teamIndia,R.color.black);
+        TeamAdapter Adapter = new TeamAdapter(this, teamIndia, R.color.black);
 
         ListView listView = (ListView) findViewById(R.id.list);
 
@@ -43,11 +43,25 @@ public class MainActivity1 extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
                 TeamIndia teamindia = teamIndia.get(position);
-                mediaplayer = MediaPlayer.create(MainActivity1.this,teamindia.getAudioResourceId());
+                mediaplayer = MediaPlayer.create(MainActivity1.this, teamindia.getAudioResourceId());
                 mediaplayer.start();
+                mediaplayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mediaplayer) {
+                        releaseMediaPlayer();
+                    }
+                });
             }
         });
 
+    }
+
+    private void releaseMediaPlayer() {
+        if (mediaplayer != null) {
+            mediaplayer.release();
+
+            mediaplayer = null;
+        }
     }
 
 }
